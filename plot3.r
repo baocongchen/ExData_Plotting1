3c9b6239b751aa00178ad0cf3ed3a0b25e273ef0
baocongchen <baocongchen@yahoo.com>
2014-08-09 22:28:42 +0900

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -14,7 +14,7 @@ data$DateTime<- strptime(paste(data$Date, data$Time, sep=" "),format="%d/%m/%Y %
 png("plot3.png", width=480, height=480)
 
 #Plot the data
-plot(data$DateTime, data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+with(data,plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
 lines(data$DateTime, data$Sub_metering_2, col="red")
 lines(data$DateTime, data$Sub_metering_3, col="blue")
 legend("topright", col=c("black", "red", "blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1)
