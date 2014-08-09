11c5eaaf72c3a405a9c0d2b1722dab6f6ea019eb
baocongchen <baocongchen@yahoo.com>
2014-08-09 19:13:53 +0900

    Committed plot2

---
+++ plot2.r
@@ -0,0 +1,17 @@
+#Load Data
+data <- read.csv("household_power_consumption.txt",na.string="?",sep=";")
+
+#Filter Data
+data <- data[(data$Date=="1/2/2007" | data$Date=="2/2/2007"),]
+
+#Combine Date and Time 
+data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), format ="%d/%m/%Y %H:%M:%S")
+
+#Open png device
+png("plot2.png", width=480, height=480)
+
+#Plot the data
+plot(data$DateTime, data$Global_active_power, type="1",xlab="",ylab="Global Active Power (kilowatts)")
+
+#Turn off png device
+dev.off()
